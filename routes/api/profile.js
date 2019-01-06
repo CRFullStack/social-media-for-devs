dad0482d595a5c36ad252206fbef2c74d667da93
CRFullStack <hobdy.chris02@gmail.com>
2019-01-06 17:48:20 -0500

    added a profile route with validation. Can now create/update profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,8 @@ const passport = require("passport");
 const Profile = require("../../models/Profile");
 // Load user model
 const Users = require("../../models/User");
+// Load validation
+const validateProfileInput = require("../../validation/profile");
 
 router.get("/test", (req, res) =>
   res.json({
@@ -14,6 +16,9 @@ router.get("/test", (req, res) =>
   })
 );
 
+//@route get api/profile/
+//@desc retrieves the user profile
+//@access Private
 router.get(
   "/",
   passport.authenticate("jwt", { session: false }),
@@ -32,6 +37,68 @@ router.get(
         res.json(profile);
       })
       .catch(err => res.status(404).json(err));
+  }
+);
+//@route post api/profile/
+//@desc create or update user profile
+//@access Private
+
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+
+    // check validation
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+    const profileFields = {};
+
+    profileFields.user = req.user.id;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.githubusername)
+      profileFields.githubusername = req.body.githubusername;
+
+    // Skills
+    if (typeof req.body.skills !== "undefined") {
+      profileFields.skills = req.body.skills.split(",");
+    }
+
+    // Social
+    profileFields.social = {};
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      if (profile) {
+        // Update profile
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ).then(profile => res.json(profile));
+      } else {
+        // Create profile
+        // check if handle exist
+        Profile.findOne({ handle: profileFields.handle }).then(profile => {
+          if (profile) {
+            errors.handle = "That handle already exists";
+            res.status(400).json(errors);
+          }
+
+          // Save profile
+          new Profile(profileFields).save().then(profile => res.json(profile));
+        });
+      }
+    });
   }
 );
 
